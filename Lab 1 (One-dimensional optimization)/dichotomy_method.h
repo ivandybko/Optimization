99f1c0f622f2bb6dc910e8f2648e23c06915fc39
Ivan Dybko <dyybkoie@student.bmstu.ru>
2025-02-22 13:22:38 +0300

    Dichotomy method implemented

--- Lab 1 (One-dimensional optimization)/dichotomy_method.h
+++ Lab 1 (One-dimensional optimization)/dichotomy_method.h
@@ -4,5 +4,29 @@
 
 #ifndef DICHOTOMY_METHOD_H
 #define DICHOTOMY_METHOD_H
-
+template <typename T>
+std::pair<T,T> dichotomy_method(std::function<T(T)> f, T a, T b, T delta=5*1e-11, T tol = 1e-10, bool iterations=false)
+{
+	T interval = b-a; T x1, x2, f_1, f_2, iter=0;;
+	while(interval > tol)
+	{
+		x1=(a+b)/2 - delta; x2=(a+b)/2 + delta;
+		f_1=f(x1); f_2=f(x2);
+		if(f_1 < f_2)
+		{
+			b=x2;
+		}
+		else
+		{
+			a=x1;
+		}
+		interval=b-a;
+		iter++;
+	}
+	if (iterations)
+	{
+		std::cout << "Convergence after " << iter << " iterations" << std::endl;
+	}
+	return std::make_pair((a+b)/2,f((a+b)/2));
+}
 #endif //DICHOTOMY_METHOD_H
--- Lab 1 (One-dimensional optimization)/main.cpp
+++ Lab 1 (One-dimensional optimization)/main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
-
+#include <cmath>
+#include <functional>
+#include "dichotomy_method.h"
+#include "golden_section_search.h"
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main()
 {
-
+	std::function<double(double)> test1 = [](double x) {
+		double R1 = std::asin((35 * std::pow(x, 2) - 30 * x + 9) / 20);
+		double R2 = std::cos((10 * std::pow(x, 3) + 185 * std::pow(x, 2) + 340 * x + 103) /
+							  (50 * std::pow(x, 2) + 100 * x + 30));
+		return R1 + R2 + 0.5;
+	};
+	auto min = dichotomy_method<double>(test1, 0.0, 1.0, 1e-7,1e-6, true);
+	std::cout << "Dichotomy method: (x=" << min.first << ", f(x)=" << min.second << ")" <<  std::endl;
 	return 0;
 }
 
