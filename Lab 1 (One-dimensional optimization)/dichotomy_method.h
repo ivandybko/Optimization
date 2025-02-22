e899488bae143abfe883b240ad1475fd62f716d5
Ivan Dybko <dyybkoie@student.bmstu.ru>
2025-02-22 18:15:22 +0300

    Dichotomy method fix

--- Lab 1 (One-dimensional optimization)/dichotomy_method.h
+++ Lab 1 (One-dimensional optimization)/dichotomy_method.h
@@ -7,8 +7,8 @@
 template <typename T>
 std::pair<T,T> dichotomy_method(std::function<T(T)> f, T a, T b, T delta=5*1e-11, T tol = 1e-10, bool iterations=false)
 {
-	T interval = b-a; T x1, x2, f_1, f_2, iter=0;;
-	while(interval > tol)
+	T x1, x2, f_1, f_2, iter=0;;
+	while((b-a) > tol)
 	{
 		x1=(a+b)/2 - delta; x2=(a+b)/2 + delta;
 		f_1=f(x1); f_2=f(x2);
@@ -20,12 +20,11 @@ std::pair<T,T> dichotomy_method(std::function<T(T)> f, T a, T b, T delta=5*1e-11
 		{
 			a=x1;
 		}
-		interval=b-a;
 		iter++;
 	}
 	if (iterations)
 	{
-		std::cout << "Convergence after " << iter << " iterations" << std::endl;
+		std::cout << "Convergence after " << iter << " iterations and " << 2*iter << " function calculations" <<  std::endl;
 	}
 	return std::make_pair((a+b)/2,f((a+b)/2));
 }
