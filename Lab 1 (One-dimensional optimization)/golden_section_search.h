21e6cdd0055c062624844c1dc6cb8dc2529ea834
Ivan Dybko <dyybkoie@student.bmstu.ru>
2025-02-23 12:14:51 +0300

    Aligned with Harvard style

--- Lab 1 (One-dimensional optimization)/golden_section_search.h
+++ Lab 1 (One-dimensional optimization)/golden_section_search.h
@@ -1,7 +1,9 @@
 #ifndef GOLDEN_SECTION_SEARCH_H
 #define GOLDEN_SECTION_SEARCH_H
+
+#include <cmath>
 template <typename T>
-std::pair<T,T> golden_secion_search(std::function<T(T)> f, T a, T b, T tol = 1e-10, bool iterations=false)
+std::pair<T,T> golden_section_search(std::function<T(T)> func, T left, T right, T tol, bool show_iterations=false)
 {
 	T ratio=(1+std::sqrt(5))/2, x1 = b-(b-a)/ratio, x2=a+(b-a)/ratio, f_1=f(x1), f_2=f(x2), iter=1;;
 	while((b-a) > tol)
@@ -10,23 +12,32 @@ std::pair<T,T> golden_secion_search(std::function<T(T)> f, T a, T b, T tol = 1e-
 		{
 			b=x2;
 			x2 = x1;
+	T ratio = (1 + std::sqrt(5)) / 2;
+	T x_1 = right - ((right - left) / ratio);
+	T x_2 = left + ((right - left) / ratio);
+	T f_1 = func(x_1);
+	T f_2 = func(x_2);
+	T iteration_count = 1;
+	while((right - left) > tol){
+		if (f_1 < f_2){
+			right=x_2;
+			x_2 = x_1;
 			f_2 = f_1;
-			x1=a+b-x2;
-			f_1=f(x1);
+			x_1 = left+right-x_2;
+			f_1 = func(x_1);
 		}
-		else
-		{
-			a=x1;
-			x1 = x2;
+		else{
+			left=x_1;
+			x_1 = x_2;
 			f_1 = f_2;
-			x2=a+b-x1;
-			f_2=f(x2);
+			x_2 = left+right-x_1;
+			f_2 = func(x_2);
 		}
-		iter++;
+		iteration_count++;
+		outFile << left << " " << right << std::endl;
 	}
-	if (iterations)
-	{
-		std::cout << "Convergence after " << iter << " iterations " << 1+iter << " function calculations" <<  std::endl;
+	if (show_iterations){
+		std::cout << "Convergence after " << iteration_count << " iterations " << 1+iteration_count << " function calculations" <<  std::endl;
 	}
 	return std::make_pair((a+b)/2,f((a+b)/2));
 }
