e5bebd3176cfe0337e125e56bfb2c456d3cd3740
Ivan Dybko <dyybkoie@student.bmstu.ru>
2025-02-23 12:15:26 +0300

    Aligned with Harvard style

--- Lab 1 (One-dimensional optimization)/dichotomy_method.h
+++ Lab 1 (One-dimensional optimization)/dichotomy_method.h
@@ -4,8 +4,9 @@
 
 #ifndef DICHOTOMY_METHOD_H
 #define DICHOTOMY_METHOD_H
+#include <fstream>
 template <typename T>
-std::pair<T,T> dichotomy_method(std::function<T(T)> f, T a, T b, T delta=5*1e-11, T tol = 1e-10, bool iterations=false)
+std::pair<T,T> dichotomy_method(std::function<T(T)> func, T left, T right, T delta, T tol, bool show_iterations = false)
 {
 	T x1, x2, f_1, f_2, iter=0;;
 	while((b-a) > tol)
@@ -15,16 +16,27 @@ std::pair<T,T> dichotomy_method(std::function<T(T)> f, T a, T b, T delta=5*1e-11
 		if(f_1 < f_2)
 		{
 			b=x2;
+	T x_1{0};
+	T x_2{0};
+	T f_1{0};
+	T f_2{0};
+	T iteration_count{0};
+	while((right - left) > tol){
+		x_1 = (left + right) / 2 - delta;
+		x_2 = (left + right) / 2 + delta;
+		f_1 = func(x_1);
+		f_2 = func(x_2);
+		if(f_1 < f_2){
+			right=x_2;
 		}
-		else
-		{
-			a=x1;
+		else{
+			left=x_1;
 		}
-		iter++;
+		iteration_count++;
+		outFile << left << " " << right << std::endl;
 	}
-	if (iterations)
-	{
-		std::cout << "Convergence after " << iter << " iterations and " << 2*iter << " function calculations" <<  std::endl;
+	if (show_iterations){
+		std::cout << "Convergence after " << iteration_count << " iterations and " << 2*iteration_count << " function calculations" <<  std::endl;
 	}
 	return std::make_pair((a+b)/2,f((a+b)/2));
 }
