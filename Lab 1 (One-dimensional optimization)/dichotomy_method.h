ee0ac926a8c15a334b85df05814b5fe6fee6944b
Ivan Dybko <ivandybfk12@gmail.com>
2025-03-23 14:27:10 +0300

    Update dichotomy_method.h

--- Lab 1 (One-dimensional optimization)/dichotomy_method.h
+++ Lab 1 (One-dimensional optimization)/dichotomy_method.h
@@ -8,14 +8,12 @@
 template <typename T>
 std::pair<T,T> dichotomy_method(std::function<T(T)> func, T left, T right, T delta, T tol, bool show_iterations = false)
 {
-	std::ofstream outFile("/Users/ivandybko/Projects/Optimization/Lab 1 (One-dimensional optimization)/data/dichotomy_method_17.txt");
 	T x_1{0};
 	T x_2{0};
 	T f_1{0};
 	T f_2{0};
 	T iteration_count{0};
 	T func_call_count{0};
-	outFile << std::setprecision(20) << left << " " << right << '\n';
 	while((right - left) > tol){
 		x_1 = (left + right) / 2 - delta;
 		x_2 = (left + right) / 2 + delta;
@@ -29,16 +27,11 @@ std::pair<T,T> dichotomy_method(std::function<T(T)> func, T left, T right, T del
 		else{
 			left=x_1;
 		}
-		// {
-		// 	throw std::invalid_argument("The delta was selected incorrectly");
-		// }
 		iteration_count++;
-		outFile << std::setprecision(20) << x_1 << " " << x_2 << '\n';
 	}
 	if (show_iterations){
 		std::cout << "Convergence after " << iteration_count << " iterations and " << func_call_count << " function calculations" <<  std::endl;
 	}
-	outFile.close();
 	return {(left + right) / 2,func((left + right) / 2)};
 }
 #endif //DICHOTOMY_METHOD_H
